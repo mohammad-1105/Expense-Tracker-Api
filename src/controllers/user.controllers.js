6ca025c1f08a0e9b0114420575c45d38c99d6ae8
Mohammad Aman <mohammadamaan1105@gmail.com>
2024-06-22 09:27:01 +0545

    add some user controllers

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -4,6 +4,8 @@ import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { registerUserSchema } from "../schemas/registerUserSchema.js";
 import { loginUserSchema } from "../schemas/loginUserSchema.js";
+import { changePasswordSchema } from "../schemas/changePasswordSchema.js";
+import { updateFullNameSchema } from "../schemas/updateFullNameSchema.js";
 
 // Global functions
 const generateAccessAndRefreshAccessTokens = async (userId) => {
@@ -156,5 +158,150 @@ const logoutUser = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, "User logged out successfully", []));
 });
 
+const refreshTokens = asyncHandler(async (req, res) => {
+  // Ensure user is authenticated and available in req.user
+  if (!req.user || !req.user._id) {
+    throw new ApiError(401, "Unauthorized access", []);
+  }
+
+  // Generate new access and refresh tokens
+  const { accessToken, refreshAccessToken } =
+    await generateAccessAndRefreshAccessTokens(req.user._id);
+
+  // Send response with new access and refresh tokens
+  return res
+    .status(200)
+    .cookie("accessToken", accessToken, cookieOptions)
+    .cookie("refreshAccessToken", refreshAccessToken, cookieOptions)
+    .json(
+      new ApiResponse(200, "Tokens refreshed successfully", {
+        accessToken,
+        refreshAccessToken,
+      })
+    );
+});
+
+const getUserProfile = asyncHandler(async (req, res) => {
+  // Ensure user is authenticated and available in req.user
+  if (!req.user || !req.user._id) {
+    throw new ApiError(401, "Unauthorized access", []);
+  }
+
+  // Find user
+  const user = await UserModel.findById(req.user._id).select(
+    "-password -refreshAccessToken"
+  );
+  if (!user) {
+    throw new Error(404, "User not found", []);
+  }
+
+  // return response
+  return res
+    .status(200)
+    .json(new ApiResponse(200, "User profile fetched successfully", user));
+});
+
+const updateUserFullName = asyncHandler(async (req, res) => {
+  // Ensure user is authenticated and available in req.user
+  if (!req.user || !req.user._id) {
+    throw new ApiError(401, "Unauthorized access", []);
+  }
+
+  // get FullName from request
+  const { fullName } = req.body;
+
+  // validation with zod
+  const { error } = updateFullNameSchema.safeParse({fullName})
+  if(error){
+    throw new ApiError(400, error.issues[0].message, [])
+  }
+
+  // Find user and update profile
+  const user = await UserModel.findByIdAndUpdate(
+    req.user._id,
+    {
+      fullName,
+    },
+    {
+      new: true,
+      runValidators: true,
+    }
+  ).select("-password -refreshAccessToken");
+  if (!user) {
+    throw new Error(404, "User not found", []);
+  }
+
+  // return response
+  return res
+    .status(200)
+    .json(new ApiResponse(200, "Name updated successfully", user));
+});
+
+const deleteUserAccount = asyncHandler(async (req, res) => {
+  // Ensure user is authenticated and available in req.user
+  if (!req.user || !req.user._id) {
+    throw new ApiError(401, "Unauthorized access", []);
+  }
+
+  // Find user and delete account
+  const user = await UserModel.findByIdAndDelete(req.user._id);
+  if (!user) {
+    throw new Error(500, "Failed to delete account", []);
+  }
+
+  // return response
+  return res
+    .status(200)
+    .json(new ApiResponse(200, "Account deleted successfully", []));
+});
+
+const changePassword = asyncHandler(async (req, res) => {
+  // Ensure user is authenticated and available in req.user
+  if (!req.user || !req.user._id) {
+    throw new ApiError(401, "Unauthorized access", []);
+  }
+
+  // get data from request body
+  const { oldPassword, newPassword } = req.body;
+
+  // validation with zod
+  const { error } = changePasswordSchema.safeParse({
+    oldPassword,
+    newPassword,
+  });
+  if (error) {
+    throw new ApiError(400, error.issues[0].message, []);
+  }
+
+  // Find user and update password
+  const user = await UserModel.findById(req.user._id);
+  if (!user) {
+    throw new Error(404, "User not found", []);
+  }
+
+  // check if the old password is correct
+  const isPasswordMatched = await user.isPasswordMatched(oldPassword);
+  if (!isPasswordMatched) {
+    throw new ApiError(401, "Invalid old password", []);
+  }
+
+  // update password
+  user.password = newPassword;
+  await user.save({ validateBeforeSave: false });
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, "Password changed successfully", []));
+});
+
 // Exports controllers
-export { registerUser, loginUser, logoutUser };
+export {
+  registerUser,
+  loginUser,
+  logoutUser,
+  refreshTokens,
+  getUserProfile,
+  updateUserFullName,
+  deleteUserAccount,
+  changePassword
+};
