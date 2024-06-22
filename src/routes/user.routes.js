6e0bf0819cec89efacbd1be5330665336a4ed7da
Mohammad Aman <mohammadamaan1105@gmail.com>
2024-06-22 09:27:09 +0545

    add some user routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,10 +1,24 @@
 import { Router } from "express";
-import { loginUser, logoutUser, registerUser } from "../controllers/user.controllers.js";
+import {
+    changePassword,
+    deleteUserAccount,
+  getUserProfile,
+  loginUser,
+  logoutUser,
+  refreshTokens,
+  registerUser,
+  updateUserFullName,
+} from "../controllers/user.controllers.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 export const router = Router();
 
-// create users endpoints 
-router.route('/register').post(registerUser)
-router.route("/login").post(loginUser)
-router.route("/logout").post(verifyJWT, logoutUser)
+// create users endpoints
+router.route("/register").post(registerUser);
+router.route("/login").post(loginUser);
+router.route("/logout").post(verifyJWT, logoutUser);
+router.route("/refresh-tokens").post(verifyJWT, refreshTokens);
+router.route("/profile").get(verifyJWT, getUserProfile);
+router.route("/profile").patch(verifyJWT, updateUserFullName);
+router.route("/delete").delete(verifyJWT, deleteUserAccount);
+router.route("/change-password").patch(verifyJWT, changePassword);
